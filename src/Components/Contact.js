4eb8fbf5169d260f2a319d5d66fd24f65346a073
Atif7355 <78923890+Atif7355@users.noreply.github.com>
2021-06-28 16:26:04 +0530

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -12,7 +12,7 @@ export const Contact = () => {
         <div className="container">
         <div className="left">
             <h1 style={{transform:"translate(90px,100px)",color:"brown",fontFamily:"sans-serif",fontSize:"50px"}}>Mohammad Atif</h1>
-            <h2 style={{transform:"translate(80px,50px)",color:"green",fontSize:"50px"}}>Mobile-<img src={mobile} alt="Gmail" width="50px" style={{transform:"translate(1px,10px)"}}></img>:7355612109</h2>
+            <h2 style={{transform:"translate(20px,50px)",color:"green",fontSize:"50px"}}>Mobile-<img src={mobile} alt="Gmail" width="50px" style={{transform:"translate(1px,10px)"}}></img>:7355612109</h2>
             <img src={contact} alt="Gmail" width="300px" style={{transform:"translate(150px,30px)"}}></img>
             <a href="mailto:sayyedmohdatif123@gmail.com">
             <img src={gmail} alt="Gmail" width="50" style={{transform:"translate(-200px,120px)",cursor:"arrow"}}></img>
@@ -25,14 +25,14 @@ export const Contact = () => {
             </a>
         </div>
         <div className="right">
-        <h1 style={{transform:"translate(95px,100px)",color:"blue",fontFamily:"sans-serif",fontSize:"50px"}}>Aman Pal Singh Rana</h1>
-        <h2 style={{transform:"translate(85px,50px)",color:"green",fontSize:"50px"}}>Mobile-<img src={mobile} alt="Gmail" width="50px" style={{transform:"translate(1px,10px)"}}></img>:7706059995</h2>
+        <h1 style={{transform:"translate(30px,100px)",color:"blue",fontFamily:"sans-serif",fontSize:"50px"}}>Aman Pal Singh Rana</h1>
+        <h2 style={{transform:"translate(20px,50px)",color:"green",fontSize:"50px"}}>Mobile-<img src={mobile} alt="Gmail" width="50px" style={{transform:"translate(1px,10px)"}}></img>:7706059995</h2>
             <img src={contact1} alt="Gmail" width="250px" style={{transform:"translate(150px,30px)"}}></img>
             <a href="mailto:amanpalsinghrana27@gmail.com">
-            <img src={gmail} alt="Gmail" width="50" style={{transform:"translate(-120px,100px)",cursor:"arrow"}}></img>
+            <img src={gmail} alt="Gmail" width="50" style={{transform:"translate(-120px,120px)",cursor:"arrow"}}></img>
             </a>
             <a href="https://www.linkedin.com/in/aman-pal-singh-rana-180837203/">
-            <img src={linkedin} alt="Gmail" width="50" style={{transform:"translate(0px,110px)",cursor:"arrow",display:"inline-block"}}></img>
+            <img src={linkedin} alt="Gmail" width="50" style={{transform:"translate(0px,130px)",cursor:"arrow",display:"inline-block"}}></img>
             </a>
 
         </div>
