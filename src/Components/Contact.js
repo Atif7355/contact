c336e690a59117754594608f2e37a8f57547f463
Atif7355 <78923890+Atif7355@users.noreply.github.com>
2021-06-28 16:34:34 +0530

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -11,9 +11,9 @@ export const Contact = () => {
         <>
         <div className="container">
         <div className="left">
-            <h1 style={{transform:"translate(90px,100px)",color:"brown",fontFamily:"sans-serif",fontSize:"50px"}}>Mohammad Atif</h1>
-            <h2 style={{transform:"translate(20px,50px)",color:"green",fontSize:"50px"}}>Mobile-<img src={mobile} alt="Gmail" width="50px" style={{transform:"translate(1px,10px)"}}></img>:7355612109</h2>
-            <img src={contact} alt="Gmail" width="300px" style={{transform:"translate(150px,30px)"}}></img>
+            <h1 style={{transform:"translate(90px,100px)",color:"brown",fontFamily:"sans-serif",fontSize:"30px"}}>Mohammad Atif</h1>
+            <h2 style={{transform:"translate(15px,70px)",color:"green",fontSize:"30px"}}>Mobile-<img src={mobile} alt="Gmail" width="30px" style={{transform:"translate(1px,10px)"}}></img>:7355612109</h2>
+            <img src={contact} alt="Gmail" width="300px" style={{transform:"translate(130px,60px)"}}></img>
             <a href="mailto:sayyedmohdatif123@gmail.com">
             <img src={gmail} alt="Gmail" width="50" style={{transform:"translate(-200px,120px)",cursor:"arrow"}}></img>
             </a>
@@ -25,9 +25,9 @@ export const Contact = () => {
             </a>
         </div>
         <div className="right">
-        <h1 style={{transform:"translate(30px,100px)",color:"blue",fontFamily:"sans-serif",fontSize:"50px"}}>Aman Pal Singh Rana</h1>
-        <h2 style={{transform:"translate(20px,50px)",color:"green",fontSize:"50px"}}>Mobile-<img src={mobile} alt="Gmail" width="50px" style={{transform:"translate(1px,10px)"}}></img>:7706059995</h2>
-            <img src={contact1} alt="Gmail" width="250px" style={{transform:"translate(150px,30px)"}}></img>
+        <h1 style={{transform:"translate(30px,100px)",color:"blue",fontFamily:"sans-serif",fontSize:"30px"}}>Aman Pal Singh Rana</h1>
+        <h2 style={{transform:"translate(20px,70px)",color:"green",fontSize:"30px"}}>Mobile-<img src={mobile} alt="Gmail" width="30px" style={{transform:"translate(1px,10px)"}}></img>:7706059995</h2>
+            <img src={contact1} alt="Gmail" width="250px" style={{transform:"translate(110px,60px)"}}></img>
             <a href="mailto:amanpalsinghrana27@gmail.com">
             <img src={gmail} alt="Gmail" width="50" style={{transform:"translate(-120px,120px)",cursor:"arrow"}}></img>
             </a>
